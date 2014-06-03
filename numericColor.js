bd87299a24650698e40464bfa122977fc8308757
danielrabelo <administrador@danielrabelo.com>
2014-06-03 19:13:21 -0300

    Biblioteca JavaScript
    Colorir celulas de um table component

---
+++ numericColor.js
@@ -0,0 +1,39 @@
+/** start addIn numericColor **/
+ var numericColor = {
+    name: "numericColor",
+    label: "numeric Color",
+    defaults: {
+    	decimal: true,
+		textFormat: function(v, st, opt, objectHtml) {		
+			var value= v;
+		    
+            if (value < 500) {
+              $(objectHtml).css({'background-color':'red'});
+        	  return "<span style='text-align:right;'>"+value+"</span>";  
+              
+            } else if (value < 1000){
+              $(objectHtml).css({'background-color':'blue'});
+              return "<span style='text-align:right;'>"+value+"</span>"; 
+              
+            } else {
+              $(objectHtml).css({'background-color':'green'});
+              return "<span style='text-align:right;'>"+value+"</span>";                 
+            }
+		
+		}
+    },
+    init: function(){
+      $.fn.dataTableExt.oSort[this.name+'-asc'] = $.fn.dataTableExt.oSort['numeric-asc'];
+      $.fn.dataTableExt.oSort[this.name+'-desc'] = $.fn.dataTableExt.oSort['numeric-desc'];
+    },
+    
+    implementation: function(tgt, st, opt){
+      var text = opt.textFormat.call(this, st.value, st, opt, tgt);
+      $(tgt).empty().append(text);
+     
+     
+    }
+    
+  };
+  Dashboards.registerAddIn("Table", "colType", new AddIn(numericColor));
+/** end addIn numericColor **/
